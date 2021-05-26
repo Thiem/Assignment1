4a6834f1fb67193fbe69cf584f2ae2f10479d93d
Thiem <tranthiem1993@gmail.com>
2021-05-26 19:25:31 +0700

    Assignment1

---
+++ Assignment01/Assignment01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    class Program
+    {    
+        /// Bai 2.
+        /// <summary>
+        /// <param name="args">comment</param>
+        /// <returns>comment</returns>
+        /// </summary>
+        /// <remarks>
+        /// comment
+        /// </remarks>
+
+        static void Main(string[] args)
+        {
+            //Bai 1.
+            int studentID = 1;
+            String studentName = "David Deogre";
+            int age = 18;
+            char gender = 'M';
+            float percent = 75.50f;
+            Console.WriteLine($"Student ID: {studentID}");
+            Console.WriteLine($"Student Name: {studentName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Percentage: {percent}");
+
+            //Bai 3.
+            int english;
+            Console.Write("Enter name of student: ");
+            string studentName1 = Console.ReadLine();
+            Console.Write("Enter marks of maths: ");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter marks of english: ");
+            english = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter marks of physical: ");
+            int physical = Convert.ToInt32(Console.ReadLine());
+            Student(studentName1, math, english, physical);
+
+
+            Console.ReadKey();
+        }
+
+        static void Student(String studentName, int math, int english, int physical)
+        {
+            int constant = 100;
+            float percent = 0f;
+            percent = ((math + english + physical) * constant) / 300;
+            Console.WriteLine($"Student Name: {studentName}");
+            Console.WriteLine($"Marks obtaned in Math: {math}");
+            Console.WriteLine($"Marks obtaned in English: {english}");
+            Console.WriteLine($"Marks obtaned in Physical: {physical}");
+            Console.WriteLine($"Percent: {percent}");
+        }
+    }
+}
---
+++ Assignment01/Phan2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phan2
+{
+    class Program
+    {
+         static void Main()
+        {
+
+            //Bai tap 1:
+            //int radius = 4;
+            //const double PI = 3.14159;
+            //double circum, area;
+            //area = PI * radius * radius;
+            //circum = 2 * PI * radius;
+            //// in kết quả
+            //Console.WriteLine($"Ban kinh = {radius}, PI = {PI}"); 
+            //Console.WriteLine($"Dien tich {area}");
+            //Console.WriteLine($"Chu vi {circum}");
+
+
+            // Bai tap 3:
+            //int x, y;
+            //for (x = 0; x < 10; x++, System.Console.Write("\n")) ;
+            //for (y = 0; y < 10; y++, System.Console.WriteLine("{0}",y)) ;    
+
+            //Bai tap 5:
+            Console.WriteLine("Xin chao");
+            Console.WriteLine("Tam biet");
+
+            //Bai tap 6:
+            Console.WriteLine("Xin chao"); //Console.WriteLine('Xin chao');
+            Console.WriteLine("Tam biet"); //Consoile.WriteLine('Tam biet');
+
+            //Bai tap 7:
+            Console.OutputEncoding = Encoding.UTF8;
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\tRằm tháng giêng");
+            Console.WriteLine(" Rằm xuân lồng lộng trăng soi,\n Sông xuân nước lẫn màu trời thêm\n xuân. Giữa dòng bàn bạc việc quân\n Khuya về bát ngát trăng ngân đầy thuyền.");
+            Console.ReadKey();
+        }
+    }
+}
